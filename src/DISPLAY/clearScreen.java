fecd1fbf1a87bdeaca3b770c8a7816ee8068bcf8
bryngrl <geroliabryan024@gmail.com>
2024-12-16 13:12:34 +0800

    adding first month reset

--- src/DISPLAY/clearScreen.java
+++ src/DISPLAY/clearScreen.java
@@ -1,7 +1,6 @@
 package display;
 
-
-public class ClearScreen {
+public class clearScreen {
     public void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
--- src/account/Login.java
+++ src/account/Login.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.MonthDay;
+import display.Date;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
@@ -18,6 +19,11 @@ public class Login {
     private Scanner s;
     boolean loggedIn = false;
     Needs needs = new Needs();
+
+
+
+
+
 
     public Login() {
         s = new Scanner(System.in);
@@ -124,28 +130,27 @@ public class Login {
     private void checkDueDates(ArrayList<String> userTxtFile) {
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    
+
         String[] dueDates = { "Electricity", "Water", "Rent", "Internet" };
-    
+
         for (int i = 4; i < userTxtFile.size() && i < 8; i++) {
             String dueDate = userTxtFile.get(i);
-            String status = userTxtFile.get(i + 4); 
-    
+            String status = userTxtFile.get(i + 4);
+
             if (!dueDate.equalsIgnoreCase("n/a") && !status.equalsIgnoreCase("true")) {
                 try {
                     LocalDate parsedDate = LocalDate.parse(dueDate, formatter);
                     MonthDay dueMonthDay = MonthDay.from(parsedDate);
                     MonthDay todayMonthDay = MonthDay.from(today);
-    
+
                     LocalDate thisYearDueDate = parsedDate.withYear(today.getYear());
                     long daysUntilDue = java.time.temporal.ChronoUnit.DAYS.between(today, thisYearDueDate);
-    
 
                     if (dueMonthDay.equals(todayMonthDay)) {
                         System.out.println("Reminder: " + dueDates[i - 4] + " payment is due today!");
-                    }
-                    else if (daysUntilDue > 0 && daysUntilDue <= 7) {
-                        System.out.println("Reminder: " + dueDates[i - 4] + " payment is due in " + daysUntilDue + " day(s).");
+                    } else if (daysUntilDue > 0 && daysUntilDue <= 7) {
+                        System.out.println(
+                                "Reminder: " + dueDates[i - 4] + " payment is due in " + daysUntilDue + " day(s).");
                     }
                 } catch (DateTimeParseException e) {
                     System.out.println("Warning: Invalid due date format for " + dueDates[i - 4]);
